a915415d51c367c64a407fd27285cb229e2a6127
Bas Baccarne <bastiaan.baccarne@ugent.be>
2025-02-03 19:23:18 +0100

    Add state management and drawing functions to init.py

--- tests/init.py
+++ tests/init.py
@@ -1,6 +1,7 @@
 # This script ...
 
 import pygame
+import random
 import time
 from pathlib import Path
 
@@ -18,6 +19,7 @@ FONT_PATH = DATA_DIR / 'TT Firs Neue Regular.ttf'
 # DESIGN
 WHITE = (255, 255, 255)
 BG_COLOR = (12, 37, 84)
+LINE_COLOR = (200, 100, 50)
 FONT = pygame.font.Font(FONT_PATH, 100)
 pygame.mouse.set_visible(False)
 
@@ -27,6 +29,9 @@ SCREEN_WIDTH = screen_info.current_w
 SCREEN_HEIGHT = screen_info.current_h
 
 # GLOBAL VARIABLES
+state = "question"
+old_state = state
+
 
 # Set up the display
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT),
@@ -34,10 +39,10 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT),
                                   vsync=1)
 pygame.display.set_caption("Comon Expo Interface")
 
-# Precalculations & preload
+# Functions
 def mainQuestion():
-    line1 = FONT.render("Kweetnieoecoole", True, WHITE)
-    line2 = FONT.render("vraag", True, WHITE)
+    line1 = FONT.render("?", True, WHITE)
+    line2 = FONT.render(" ", True, WHITE)
     rotated_line1 = pygame.transform.rotate(line1, 90)
     rotated_line2 = pygame.transform.rotate(line2, 90)
     line1_rect = rotated_line1.get_rect(center=(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2))
@@ -47,10 +52,19 @@ def mainQuestion():
     screen.blit(rotated_line1, line1_rect)
     screen.blit(rotated_line2, line2_rect)
 
-    pygame.display.flip()
+def drawLine(point1, point2, point3):
+    pygame.draw.line(screen, WHITE, point1, point2, 5)
+    pygame.draw.line(screen, WHITE, point2, point3, 5)
+    
 
-
+# Preload
 mainQuestion()
+drawLine((100,0),(400,SCREEN_HEIGHT//4),(SCREEN_WIDTH//2-200,SCREEN_HEIGHT//2))
+drawLine((100,SCREEN_HEIGHT),(400,SCREEN_HEIGHT-SCREEN_HEIGHT//4),(SCREEN_WIDTH//2-200,SCREEN_HEIGHT//2))
+drawLine((SCREEN_WIDTH-100,SCREEN_HEIGHT),(SCREEN_WIDTH-400,SCREEN_HEIGHT-SCREEN_HEIGHT//4),(SCREEN_WIDTH//2+100,SCREEN_HEIGHT//2))
+
+pygame.display.flip()
+
 
 # Animation loop
 clock = pygame.time.Clock()
@@ -60,6 +74,36 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
+
+        # state swicher
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            state = "response" if state == "question" else "question"
+            print(f"State: {state}")
+            if state == "question":
+                mainQuestion()
+                drawLine((100,0),(400,SCREEN_HEIGHT//4),(SCREEN_WIDTH//2-200,SCREEN_HEIGHT//2))
+                drawLine((100,SCREEN_HEIGHT),(400,SCREEN_HEIGHT-SCREEN_HEIGHT//4),(SCREEN_WIDTH//2-200,SCREEN_HEIGHT//2))
+                drawLine((SCREEN_WIDTH-100,SCREEN_HEIGHT),(SCREEN_WIDTH-400,SCREEN_HEIGHT-SCREEN_HEIGHT//4),(SCREEN_WIDTH//2+100,SCREEN_HEIGHT//2))
+
+                pygame.display.flip()
+
+            elif state == "response":
+                screen.fill(BG_COLOR)
+                
+                rect_agree = pygame.Rect(0, 0, 500, 400)
+                rect_agree.center = (SCREEN_WIDTH // 1.3 , SCREEN_HEIGHT // 2)
+
+                rect_disagree = pygame.Rect(0, 0, 450, 400)
+                rect_disagree.center = (SCREEN_WIDTH //2 , SCREEN_HEIGHT // 2)
+
+                rect_noopinion = pygame.Rect(0, 0, 600, 400)
+                rect_noopinion.center = (400 , SCREEN_HEIGHT // 2)
+
+                pygame.draw.rect(screen, WHITE, rect_agree, border_radius=40)
+                pygame.draw.rect(screen, WHITE, rect_disagree, border_radius=40)
+                pygame.draw.rect(screen, WHITE, rect_noopinion, border_radius=40)
+                pygame.display.flip()
+    
 
     # Do stuff
 
