19035074b1b34e6b2a5897e7474ed182def4f710
Bas Baccarne <bastiaan.baccarne@ugent.be>
2025-02-17 18:46:47 +0100

    refactor response text positioning for improved alignment

--- src/main.py
+++ src/main.py
@@ -327,11 +327,11 @@ def response_screen():
     screen.blit(geenmening_2, label_geenmening_position)
     screen.blit(nietakkoord_2, label_nietakkoord_position)
     if pressed == 1:
-        screen.blit(jouwmening, (rect_agree.center[0] + size_ovals[1]//2-50 ,120))
+        screen.blit(jouwmening, (label_akkoord_position[0] ,120))
     if pressed == 3:
-        screen.blit(jouwmening, (rect_noopinion.center[0] + size_ovals[1]//2-50,120))
+        screen.blit(jouwmening, (label_geenmening_position,120))
     if pressed == 2:
-        screen.blit(jouwmening, (rect_disagree.center[0] + size_ovals[1]//2-50,120))
+        screen.blit(jouwmening, (label_nietakkoord_position,120))
 
     # load progress bar (20 seconds)
     init_Progress_bar(20)
